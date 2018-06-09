21b2656edbda5d476d7e587d5148cdc3708507d9
Jingchuan Huang <hjc@hjc.im>
2018-06-09 14:37:36 +0800

    Add NanoPC T4 support.

--- config/bootscripts/boot-rk3399.cmd
+++ config/bootscripts/boot-rk3399.cmd
@@ -22,7 +22,7 @@ fi
 if test "${logo}" = "disabled"; then setenv logo "logo.nologo"; fi
 
 if test "${console}" = "display" || test "${console}" = "both"; then setenv consoleargs "console=tty1"; fi
-if test "${console}" = "serial" || test "${console}" = "both"; then setenv consoleargs "${consoleargs} earlycon=uart8250,mmio32,0xff1a0000 console=uart8250,mmio32,0xff1a0000"; fi
+if test "${console}" = "serial" || test "${console}" = "both"; then setenv consoleargs "${consoleargs} earlycon=uart8250,mmio32,0xff1a0000 console=ttyFIQ0"; fi
 
 # get PARTUUID of first partition on SD/eMMC the boot script was loaded from
 if test "${devtype}" = "mmc"; then part uuid mmc ${devnum}:1 partuuid; fi
