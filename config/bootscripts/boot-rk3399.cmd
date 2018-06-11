343054140fe3e1054f2c1923504263cfb26f9b84
Jingchuan Huang <hjc@hjc.im>
2018-06-11 14:38:58 +0800

    Add ttyargs

--- config/bootscripts/boot-rk3399.cmd
+++ config/bootscripts/boot-rk3399.cmd
@@ -9,6 +9,7 @@ setenv overlay_error "false"
 setenv rootdev "/dev/mmcblk0p1"
 setenv verbosity "1"
 setenv console "serial"
+setenv ttyargs "ttyFIQ0"
 setenv rootfstype "ext4"
 setenv docker_optimizations "on"
 
@@ -22,7 +23,7 @@ fi
 if test "${logo}" = "disabled"; then setenv logo "logo.nologo"; fi
 
 if test "${console}" = "display" || test "${console}" = "both"; then setenv consoleargs "console=tty1"; fi
-if test "${console}" = "serial" || test "${console}" = "both"; then setenv consoleargs "${consoleargs} earlycon=uart8250,mmio32,0xff1a0000 console=ttyFIQ0"; fi
+if test "${console}" = "serial" || test "${console}" = "both"; then setenv consoleargs "${consoleargs} earlycon=uart8250,mmio32,0xff1a0000 console={ttyargs}"; fi
 
 # get PARTUUID of first partition on SD/eMMC the boot script was loaded from
 if test "${devtype}" = "mmc"; then part uuid mmc ${devnum}:1 partuuid; fi
